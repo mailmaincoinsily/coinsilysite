c52a207158d8c2176c3a6ee4af54b1ae1522b5a9
alaminizaanarbitrage <134001483+alaminizaanarbitrage@users.noreply.github.com>
2023-05-19 12:16:27 +0600

    Create script.js

---
+++ static/script.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function() {
+    var tableHeader = document.querySelector('thead');
+    var rect = tableHeader.getBoundingClientRect();
+    
+    if (rect.top <= 0) {
+        tableHeader.classList.add('sticky');
+    } else {
+        tableHeader.classList.remove('sticky');
+    }
+});
