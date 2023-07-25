8ead442d2ff62a45b42cc3d9f1c5147187d1da79
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-07-25 20:40:08 +0600

    Update app.py

--- app.py
+++ app.py
@@ -39,32 +39,65 @@ def index():
         logger.error(error_message)
         return render_template('error.html', error_message=error_message)
 
-    common_symbols = set(gateio_tickers.keys()) & set(mexc_tickers.keys())
+    # Get deposit and withdrawal information for currencies on Gate.io
+    gateio_currencies = gateio.fetch_currencies()
+    if gateio_currencies is None:
+        error_message = "Failed to fetch currencies from Gate.io"
+        logger.error(error_message)
+        return render_template('error.html', error_message=error_message)
+
+    # Filter currencies with deposit and withdrawal enabled on Gate.io
+    gateio_currencies = {symbol: currency_info for symbol, currency_info in gateio_currencies.items() if 'depositEnable' in currency_info and 'withdrawEnable' in currency_info and currency_info['depositEnable'] and currency_info['withdrawEnable']}
+
+    # Get deposit and withdrawal information for currencies on MEXC Global
+    mexc_currencies = mexc_global.fetch_currencies()
+    if mexc_currencies is None:
+        error_message = "Failed to fetch currencies from MEXC Global"
+        logger.error(error_message)
+        return render_template('error.html', error_message=error_message)
+
+    # Filter currencies with deposit and withdrawal enabled on MEXC Global
+    mexc_currencies = {symbol: currency_info for symbol, currency_info in mexc_currencies.items() if 'depositEnable' in currency_info and 'withdrawEnable' in currency_info and currency_info['depositEnable'] and currency_info['withdrawEnable']}
+
+    common_symbols = set(gateio_tickers.keys()) & set(mexc_tickers.keys()) & set(gateio_currencies.keys()) & set(mexc_currencies.keys())
     logger.info("Common symbols: %s", common_symbols)
 
     data = []
     for symbol in common_symbols:
-        if symbol not in gateio.currencies:
-            continue
-
         gateio_price = float(gateio_tickers[symbol]['last'])
         mexc_price = float(mexc_tickers[symbol]['last']) if mexc_tickers[symbol]['last'] is not None else 0.0
         arbitrage = round((mexc_price - gateio_price) / gateio_price * 100, 2)
 
-        gateio_withdrawal_fee = float(gateio.currencies[symbol]['withdrawFee'])
-        mexc_withdrawal_fee = float(mexc_global.currencies[symbol]['withdrawFee'])
+        gateio_withdraw_fee = float(gateio_currencies[symbol]['withdrawFee'])
+        mexc_withdraw_fee = float(mexc_currencies[symbol]['withdrawFee'])
 
-        symbol_profit = arbitrage - gateio_withdrawal_fee - mexc_withdrawal_fee
-        symbol['profit'] = symbol_profit
+        # Calculate profit after deducting withdrawal fees
+        initial_investment = 100.0  # Your initial investment in USD or USDT
+        profit_after_fees = initial_investment * (arbitrage / 100) - gateio_withdraw_fee - mexc_withdraw_fee
 
-    # Sort data by profit value
-    data.sort(key=lambda x: x['profit'], reverse=True)
+        if profit_after_fees > 0:
+            gateio_trade_link = "https://www.gate.io/trade/{}".format(symbol.replace("/", "_"))
+            mexc_trade_link = "https://www.mexc.com/exchange/{}".format(symbol.replace("/", "_"))
 
-    # Count the number of profitable arbitrage opportunities
-    profit_count = len(data)
+            data.append({
+                'symbol': symbol,
+                'gateio_price': gateio_price,
+                'mexc_price': mexc_price,
+                'arbitrage': arbitrage,
+                'profit_after_fees': profit_after_fees,
+                'gateio_trade_link': gateio_trade_link,
+                'mexc_trade_link': mexc_trade_link
+            })
+
+    # Sort data by profit_after_fees value
+    data.sort(key=lambda x: x['profit_after_fees'], reverse=True)
+
+    # Count the number of positive and negative arbitrage opportunities
+    positive_count = sum(1 for item in data if item['arbitrage'] > 0)
+    negative_count = sum(1 for item in data if item['arbitrage'] < 0)
 
     logger.info("Data: %s", data)
-    return render_template('index.html', data=data, profit_count=profit_count)
+    return render_template('index.html', data=data, positive_count=positive_count, negative_count=negative_count)
 
 if __name__ == '__main__':
     app.run()
