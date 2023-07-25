2fd79990092a2b2fe2fb51dcddfea9fb23a45bf0
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-07-25 21:45:43 +0600

    Update app.py

--- app.py
+++ app.py
@@ -23,42 +23,6 @@ def are_contacts_matching(exchange1, symbol1, exchange2, symbol2):
     currency_info2 = exchange2.fetch_currency(symbol2)
     return currency_info1.get('contract', '') == currency_info2.get('contract', '')
 
-def are_networks_matching(exchange1, symbol1, exchange2, symbol2):
-    currency_info1 = exchange1.fetch_currency(symbol1)
-    currency_info2 = exchange2.fetch_currency(symbol2)
-    return currency_info1.get('network', '') == currency_info2.get('network', '')
-
-def get_arbitrage_opportunities(gateio, mexc_global, gateio_tickers, mexc_tickers):
-    common_symbols = set(gateio_tickers.keys()) & set(mexc_tickers.keys())
-    
-    data = []
-    for symbol in common_symbols:
-        gateio_price = float(gateio_tickers[symbol]['last'])
-        mexc_price = float(mexc_tickers[symbol]['last']) if mexc_tickers[symbol]['last'] is not None else 0.0
-        arbitrage = round((mexc_price - gateio_price) / gateio_price * 100, 2)
-
-        gateio_trade_link = "https://www.gate.io/trade/{}".format(symbol.replace("/", "_"))
-        mexc_trade_link = "https://www.mexc.com/exchange/{}".format(symbol.replace("/", "_"))
-
-        if (is_deposit_withdraw_enabled(gateio, symbol) and 
-            is_deposit_withdraw_enabled(mexc_global, symbol) and 
-            are_contacts_matching(gateio, symbol, mexc_global, symbol) and 
-            are_networks_matching(gateio, symbol, mexc_global, symbol)):
-            
-            data.append({
-                'symbol': symbol,
-                'gateio_price': gateio_price,
-                'mexc_price': mexc_price,
-                'arbitrage': arbitrage,
-                'gateio_trade_link': gateio_trade_link,
-                'mexc_trade_link': mexc_trade_link
-            })
-
-    # Sort data by arbitrage value
-    data.sort(key=lambda x: x['arbitrage'], reverse=True)
-
-    return data
-
 @app.route('/')
 def index():
     gateio = ccxt.gateio()
@@ -84,11 +48,39 @@ def index():
         logger.error(error_message)
         return render_template('error.html', error_message=error_message)
 
-    data = get_arbitrage_opportunities(gateio, mexc_global, gateio_tickers, mexc_tickers)
+    common_symbols = set(gateio_tickers.keys()) & set(mexc_tickers.keys())
+    logger.info("Common symbols: %s", common_symbols)
 
-    positive_count = sum(1 for item in data if item['arbitrage'] > 0)
-    negative_count = sum(1 for item in data if item['arbitrage'] < 0)
+    filtered_data = []
+    for symbol in common_symbols:
+        gateio_price = float(gateio_tickers[symbol]['last'])
+        mexc_price = float(mexc_tickers[symbol]['last']) if mexc_tickers[symbol]['last'] is not None else 0.0
+        arbitrage = round((mexc_price - gateio_price) / gateio_price * 100, 2)
 
+        gateio_trade_link = "https://www.gate.io/trade/{}".format(symbol.replace("/", "_"))
+        mexc_trade_link = "https://www.mexc.com/exchange/{}".format(symbol.replace("/", "_"))
+
+        if is_deposit_withdraw_enabled(gateio, symbol) and are_contacts_matching(gateio, symbol, mexc_global, symbol):
+            filtered_data.append({
+                'symbol': symbol,
+                'gateio_price': gateio_price,
+                'mexc_price': mexc_price,
+                'arbitrage': arbitrage,
+                'gateio_trade_link': gateio_trade_link,
+                'mexc_trade_link': mexc_trade_link
+            })
+
+    # Sort filtered data by arbitrage value
+    filtered_data.sort(key=lambda x: x['arbitrage'], reverse=True)
+
+    # Count the number of positive and negative arbitrage opportunities in filtered data
+    positive_count = sum(1 for item in filtered_data if item['arbitrage'] > 0)
+    negative_count = sum(1 for item in filtered_data if item['arbitrage'] < 0)
+
+    # Combine filtered data and the rest of the data
+    data = filtered_data + [item for item in data if item not in filtered_data]
+
+    logger.info("Data: %s", data)
     return render_template('index.html', data=data, positive_count=positive_count, negative_count=negative_count)
 
 if __name__ == '__main__':
