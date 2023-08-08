de882d58497f021383396861368f15daafc708b2
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-08 18:44:42 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,48 @@
 from flask import Flask, render_template, request
 from engine import calculate_arbitrage, get_exchange_name
 from config import EXCHANGES
-from coingecko import get_coingecko_price  # Import the get_coingecko_price function from coingecko.py
+from coingecko import get_coingecko_price
 
 app = Flask(__name__)
 
-@app.route('/')
+# Function to import exchange module dynamically
+def get_exchange_module(module_name):
+    return __import__(module_name)
+
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', positive_count=0, negative_count=0, exchanges=EXCHANGES)
+    return render_template('index.html', exchanges=EXCHANGES)
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
     exchange1 = request.form['exchange1']
     exchange2 = request.form['exchange2']
 
-    data = calculate_arbitrage(exchange1, exchange2)
-    
-    # Fetch Coingecko price for the selected cryptocurrency pair
-    common_symbols = set(exchange1_tickers.keys()) & set(exchange2_tickers.keys())
-    selected_symbol = common_symbols.pop() if common_symbols else None  # Get the first common symbol
-    coingecko_price = get_coingecko_price(selected_symbol)
-    
-    # Count the number of positive and negative arbitrage opportunities
-    positive_count = sum(1 for item in data if item['arbitrage'] > 0)
-    negative_count = sum(1 for item in data if item['arbitrage'] < 0)
-    
-    # Get the exchange names for display
+    # Fetch ticker data for exchange1 and exchange2
+    exchange1_config = EXCHANGES.get(exchange1)
+    exchange2_config = EXCHANGES.get(exchange2)
+
+    if not exchange1_config or not exchange2_config:
+        return "Invalid exchanges selected"
+
+    exchange1_module = get_exchange_module(exchange1_config['module'])
+    exchange2_module = get_exchange_module(exchange2_config['module'])
+
+    exchange1_tickers = exchange1_module.get_exchange_data()  # Fetch ticker data for exchange1
+    exchange2_tickers = exchange2_module.get_exchange_data()  # Fetch ticker data for exchange2
+
+    # Calculate arbitrage opportunities and related data
+    data, positive_count, negative_count = calculate_arbitrage(
+        exchange1_tickers,
+        exchange2_tickers
+    )
+
     exchange1_name = get_exchange_name(exchange1)
     exchange2_name = get_exchange_name(exchange2)
-    
+
+    # Fetch price from CoinGecko
+    coingecko_price = get_coingecko_price()
+
     return render_template(
         'index.html',
         positive_count=positive_count,
@@ -41,4 +55,4 @@ def calculate():
     )
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
