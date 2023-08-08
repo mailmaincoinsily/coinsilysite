5e9c1e15cff498e9fe6d88158d1431063c127098
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-08 16:16:20 +0600

    Update config.py

--- config.py
+++ config.py
@@ -21,17 +21,17 @@ EXCHANGES = {
     'gateio': {
         'name': 'Gate.io',
         'trade_base_url': 'https://www.gate.io/trade/',
-        'module': 'gateio',  # Name of the module (gateio.py) without the .py extension
+        'module': 'exchanges.gateio',  # Name of the module (gateio.py) without the .py extension
     },
     'mexc': {
         'name': 'MEXC',
         'trade_base_url': 'https://www.mexc.com/exchange/',
-        'module': 'mexc',
+        'module': 'exchanges.mexc',
     },
     'binance': {
         'name': 'Binance',
         'trade_base_url': 'https://www.binance.com/en/trade/',
-        'module': 'binance',
+        'module': 'exchanges.binance',
     },
     # Add more exchanges and their details here
 }
