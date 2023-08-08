0682660a4bb5b31a9aeea1968704d42995327e20
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-09 02:06:31 +0600

    Update app.py

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ def calculate():
     exchange2 = request.form['exchange2']
     liquidity_amount_str = request.form['liquidityAmount']
     
-    # Check if liquidityAmount field is not empty and is a valid numeric value
+    data = calculate_arbitrage(exchange1, exchange2)
+    positive_count = 0
+    negative_count = 0
+    valid_arbitrages = []
+    
     if liquidity_amount_str and liquidity_amount_str.replace('.', '', 1).isdigit():
         liquidity_amount = float(liquidity_amount_str)
-        data = calculate_arbitrage(exchange1, exchange2, liquidity_amount)
-        positive_count = 0
-        negative_count = 0
-        valid_arbitrages = []
         
         for item in data:
             if item['arbitrage'] > 0 and item['arbitrage'] <= liquidity_amount:
@@ -29,26 +29,24 @@ def calculate():
             elif item['arbitrage'] < 0:
                 valid_arbitrages.append(item)
                 negative_count += 1
-        
-        exchange1_name = get_exchange_name(exchange1)
-        exchange2_name = get_exchange_name(exchange2)
-        
-        return render_template(
-            'index.html',
-            positive_count=positive_count,
-            negative_count=negative_count,
-            data=valid_arbitrages,
-            exchange1_name=exchange1_name,
-            exchange2_name=exchange2_name,
-            exchanges=EXCHANGES,
-            liquidity_amount=liquidity_amount
-        )
     else:
-        return render_template(
-            'index.html',
-            error_message='Please enter a valid liquidity amount.',
-            exchanges=EXCHANGES
-        )
+        valid_arbitrages = data
+        positive_count = sum(1 for item in data if item['arbitrage'] > 0)
+        negative_count = sum(1 for item in data if item['arbitrage'] < 0)
+        
+    exchange1_name = get_exchange_name(exchange1)
+    exchange2_name = get_exchange_name(exchange2)
+    
+    return render_template(
+        'index.html',
+        positive_count=positive_count,
+        negative_count=negative_count,
+        data=valid_arbitrages,
+        exchange1_name=exchange1_name,
+        exchange2_name=exchange2_name,
+        exchanges=EXCHANGES,
+        liquidity_amount=liquidity_amount_str
+    )
 
 if __name__ == '__main__':
     app.run()
