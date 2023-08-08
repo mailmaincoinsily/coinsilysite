6bda30dca88a3b478cf20ada2cf6570be147784e
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-08 11:07:07 +0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template
+from gateio import get_gateio_data
+from mexc import get_mexc_data
+from config import app, logger
+
+@app.route('/')
+def index():
+    gateio_tickers = get_gateio_data()
+    mexc_tickers = get_mexc_data()
+
+    if gateio_tickers is None or mexc_tickers is None:
+        error_message = "Error retrieving data"
+        logger.error(error_message)
+        return render_template('error.html', error_message=error_message)
+
+    common_symbols = set(gateio_tickers.keys()) & set(mexc_tickers.keys())
+
+    data = []
+    for symbol in common_symbols:
+        gateio_price = float(gateio_tickers[symbol]['last'])
+        mexc_price = float(mexc_tickers[symbol]['last']) if mexc_tickers[symbol]['last'] is not None else 0.0
+        arbitrage = round((mexc_price - gateio_price) / gateio_price * 100, 2)
+
+        gateio_trade_link = "https://www.gate.io/trade/{}".format(symbol.replace("/", "_"))
+        mexc_trade_link = "https://www.mexc.com/exchange/{}".format(symbol.replace("/", "_"))
+
+        data.append({
+            'symbol': symbol,
+            'gateio_price': gateio_price,
+            'mexc_price': mexc_price,
+            'arbitrage': arbitrage,
+            'gateio_trade_link': gateio_trade_link,
+            'mexc_trade_link': mexc_trade_link
+        })
+
+    # Sort data by arbitrage value
+    data.sort(key=lambda x: x['arbitrage'], reverse=True)
+
+    # Count the number of positive and negative arbitrage opportunities
+    positive_count = sum(1 for item in data if item['arbitrage'] > 0)
+    negative_count = sum(1 for item in data if item['arbitrage'] < 0)
+
+    return render_template('index.html', data=data, positive_count=positive_count, negative_count=negative_count)
+
+if __name__ == '__main__':
+    app.run()
