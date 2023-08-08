343864613d87a53fc66922b08de7945b8690992a
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-08 19:08:10 +0600

    Create engine.py

---
+++ engine.py
@@ -0,0 +1,63 @@
+from importlib import import_module
+from config import EXCHANGES
+from coingecko import get_coingecko_price
+
+def get_exchange_name(exchange_key):
+    return EXCHANGES[exchange_key]['name']
+
+def get_exchange_module(exchange_key):
+    module_name = EXCHANGES[exchange_key]['module']
+    return import_module(module_name)
+
+def calculate_arbitrage(exchange1, exchange2):
+    exchange1_config = EXCHANGES.get(exchange1)
+    exchange2_config = EXCHANGES.get(exchange2)
+
+    if not exchange1_config or not exchange2_config:
+        return []
+
+    exchange1_module = get_exchange_module(exchange1_config['module'])
+    exchange2_module = get_exchange_module(exchange2_config['module'])
+
+    exchange1_tickers = exchange1_module.get_exchange_data()
+    exchange2_tickers = exchange2_module.get_exchange_data()
+
+    exchange1_trade_base_url = exchange1_config['trade_base_url']
+    exchange2_trade_base_url = exchange2_config['trade_base_url']
+
+    common_symbols = set(exchange1_tickers.keys()) & set(exchange2_tickers.keys())
+
+    data = []
+    for symbol in common_symbols:
+        exchange1_price = float(exchange1_tickers[symbol]['last'])
+        exchange2_price = float(exchange2_tickers[symbol]['last']) if exchange2_tickers[symbol]['last'] is not None else 0.0
+        arbitrage = round((exchange2_price - exchange1_price) / exchange1_price * 100, 2)
+
+        # Fetch Coingecko price for the same pair
+        coingecko_price = get_coingecko_price(symbol)
+
+        # Calculate price differences
+        exchange1_price_diff = exchange1_price - coingecko_price
+        exchange2_price_diff = exchange2_price - coingecko_price
+
+        exchange1_trade_link = "{}{}".format(exchange1_trade_base_url, symbol.replace("/", "_"))
+        exchange2_trade_link = "{}{}".format(exchange2_trade_base_url, symbol.replace("/", "_"))
+
+        data.append({
+            'symbol': symbol,
+            'exchange1_price': exchange1_price,
+            'exchange2_price': exchange2_price,
+            'arbitrage': arbitrage,
+            'coingecko_price': coingecko_price,
+            'exchange1_price_diff': exchange1_price_diff,
+            'exchange2_price_diff': exchange2_price_diff,
+            'exchange1_trade_link': exchange1_trade_link,
+            'exchange2_trade_link': exchange2_trade_link,
+            'exchange1_name': exchange1_config['name'],
+            'exchange2_name': exchange2_config['name']
+        })
+
+    # Sort data by arbitrage value
+    data.sort(key=lambda x: x['arbitrage'], reverse=True)
+
+    return data
