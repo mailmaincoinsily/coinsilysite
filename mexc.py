cc87307db515934a3997a5bf999c622cd51bd708
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-08 15:18:55 +0600

    Update mexc.py

--- mexc.py
+++ mexc.py
@@ -1,12 +1,12 @@
 import ccxt
 
-mexc_global = ccxt.mexc()
+mexc = ccxt.mexc()
 
 def get_mexc_data():
     try:
-        mexc_markets = mexc_global.load_markets()
+        mexc_markets = mexc.load_markets()
         mexc_spot_markets = {symbol: market for symbol, market in mexc_markets.items() if market['spot'] and market['active']}
-        mexc_tickers = mexc_global.fetch_tickers(list(mexc_spot_markets.keys()))
+        mexc_tickers = mexc.fetch_tickers(list(mexc_spot_markets.keys()))
         return mexc_tickers
     except Exception as e:
-        return None, "Error retrieving data from MEXC Global API: {}".format(e)
+        return None, "Error retrieving data from MEXC API: {}".format(e)
