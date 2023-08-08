42b982db86da3baf618070c07413afd3b4e7ff12
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-09 01:36:40 +0600

    Update app.py

--- app.py
+++ app.py
@@ -12,25 +12,34 @@ def index():
 def calculate():
     exchange1 = request.form['exchange1']
     exchange2 = request.form['exchange2']
-    liquidity_amount = float(request.form['liquidityAmount'])  # Get liquidity amount from the form
-
-    data = calculate_arbitrage(exchange1, exchange2, liquidity_amount)  # Pass the liquidity_amount
-
-    positive_count = sum(1 for item in data if item['arbitrage'] > 0)
-    negative_count = sum(1 for item in data if item['arbitrage'] < 0)
-
-    exchange1_name = get_exchange_name(exchange1)
-    exchange2_name = get_exchange_name(exchange2)
-
-    return render_template(
-        'index.html',
-        positive_count=positive_count,
-        negative_count=negative_count,
-        data=data,
-        exchange1_name=exchange1_name,
-        exchange2_name=exchange2_name,
-        exchanges=EXCHANGES
-    )
+    
+    liquidity_amount_str = request.form['liquidityAmount']
+    if liquidity_amount_str and liquidity_amount_str.isdigit():
+        liquidity_amount = float(liquidity_amount_str)
+        
+        data = calculate_arbitrage(exchange1, exchange2, liquidity_amount)
+        positive_count = sum(1 for item in data if item['arbitrage'] > 0)
+        negative_count = sum(1 for item in data if item['arbitrage'] < 0)
+        
+        exchange1_name = get_exchange_name(exchange1)
+        exchange2_name = get_exchange_name(exchange2)
+        
+        return render_template(
+            'index.html',
+            positive_count=positive_count,
+            negative_count=negative_count,
+            data=data,
+            exchange1_name=exchange1_name,
+            exchange2_name=exchange2_name,
+            exchanges=EXCHANGES
+        )
+    else:
+        # Handle the case where the field is empty or not a valid numeric value
+        return render_template(
+            'index.html',
+            error_message='Please enter a valid liquidity amount.',
+            exchanges=EXCHANGES
+        )
 
 if __name__ == '__main__':
     app.run()
