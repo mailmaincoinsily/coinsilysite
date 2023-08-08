31923fdbc635c501c31739f2b273d93dd93be606
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-08 15:16:25 +0600

    Update gateio.py

--- gateio.py
+++ gateio.py
@@ -2,7 +2,7 @@ import ccxt
 
 gateio = ccxt.gateio()
 
-def get_gateio_data():
+def get_exchange_data():
     try:
         gateio_markets = gateio.load_markets()
         gateio_spot_markets = {symbol: market for symbol, market in gateio_markets.items() if market['spot'] and market['active']}
