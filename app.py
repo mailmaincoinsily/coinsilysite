d6a9e6d484f63199dce73ee2bc87c99a7d8174f4
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-09 01:43:25 +0600

    Update app.py

--- app.py
+++ app.py
@@ -12,14 +12,23 @@ def index():
 def calculate():
     exchange1 = request.form['exchange1']
     exchange2 = request.form['exchange2']
-    
     liquidity_amount_str = request.form['liquidityAmount']
-    if liquidity_amount_str and liquidity_amount_str.isdigit():
+    
+    # Check if liquidityAmount field is not empty and is a valid numeric value
+    if liquidity_amount_str and liquidity_amount_str.replace('.', '', 1).isdigit():
         liquidity_amount = float(liquidity_amount_str)
+        data = calculate_arbitrage(exchange1, exchange2)
+        positive_count = 0
+        negative_count = 0
+        valid_arbitrages = []
         
-        data = calculate_arbitrage(exchange1, exchange2, liquidity_amount)
-        positive_count = sum(1 for item in data if item['arbitrage'] > 0)
-        negative_count = sum(1 for item in data if item['arbitrage'] < 0)
+        for item in data:
+            if item['arbitrage'] > 0 and item['arbitrage'] <= liquidity_amount:
+                valid_arbitrages.append(item)
+                positive_count += 1
+            elif item['arbitrage'] < 0:
+                valid_arbitrages.append(item)
+                negative_count += 1
         
         exchange1_name = get_exchange_name(exchange1)
         exchange2_name = get_exchange_name(exchange2)
@@ -28,13 +37,13 @@ def calculate():
             'index.html',
             positive_count=positive_count,
             negative_count=negative_count,
-            data=data,
+            data=valid_arbitrages,
             exchange1_name=exchange1_name,
             exchange2_name=exchange2_name,
-            exchanges=EXCHANGES
+            exchanges=EXCHANGES,
+            liquidity_amount=liquidity_amount
         )
     else:
-        # Handle the case where the field is empty or not a valid numeric value
         return render_template(
             'index.html',
             error_message='Please enter a valid liquidity amount.',
