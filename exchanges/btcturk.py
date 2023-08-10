65f7b1b7e570f04b994d073e48935d357a2d04e1
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-10 13:09:01 +0600

    Create btcturk.py

---
+++ exchanges/btcturk.py
@@ -0,0 +1,11 @@
+import ccxt
+
+def get_btcturk_data():
+    btcturk = ccxt.btcturk()
+    try:
+        btcturk_markets = btcturk.load_markets()
+        btcturk_spot_markets = {symbol: market for symbol, market in btcturk_markets.items() if market['spot'] and market['active']}
+        btcturk_tickers = btcturk.fetch_tickers(list(btcturk_spot_markets.keys()))
+        return btcturk_tickers
+    except Exception as e:
+        return None, f"Error retrieving data from BTCTurk API: {e}"
