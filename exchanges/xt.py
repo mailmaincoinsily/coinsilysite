9f6994b9fd8fa2fd609c7e2bc009b3f2677591a3
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-10 12:57:40 +0600

    Create xt.py

---
+++ exchanges/xt.py
@@ -0,0 +1,11 @@
+import ccxt
+
+def get_xt_data():
+    xt = ccxt.xt()
+    try:
+        xt_markets = xt.load_markets()
+        xt_spot_markets = {symbol: market for symbol, market in xt_markets.items() if market['spot'] and market['active']}
+        xt_tickers = xt.fetch_tickers(list(xt_spot_markets.keys()))
+        return xt_tickers
+    except Exception as e:
+        return None, f"Error retrieving data from XT API: {e}"
