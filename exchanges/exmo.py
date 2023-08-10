b8c0b0f93b7c24e08a9b5ee5c9253278034cf750
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-10 13:03:25 +0600

    Create exmo.py

---
+++ exchanges/exmo.py
@@ -0,0 +1,11 @@
+import ccxt
+
+def get_exmo_data():
+    exmo = ccxt.exmo()
+    try:
+        exmo_markets = exmo.load_markets()
+        exmo_spot_markets = {symbol: market for symbol, market in exmo_markets.items() if market['spot'] and market['active']}
+        exmo_tickers = exmo.fetch_tickers(list(exmo_spot_markets.keys()))
+        return exmo_tickers
+    except Exception as e:
+        return None, f"Error retrieving data from Exmo API: {e}"
