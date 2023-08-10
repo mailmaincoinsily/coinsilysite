1854edd0e057f9e9a771fcebbc155befe3464e95
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-10 12:56:05 +0600

    Create bitforex.py

---
+++ exchanges/bitforex.py
@@ -0,0 +1,11 @@
+import ccxt
+
+def get_bitforex_data():
+    bitforex = ccxt.bitforex()
+    try:
+        bitforex_markets = bitforex.load_markets()
+        bitforex_spot_markets = {symbol: market for symbol, market in bitforex_markets.items() if market['spot'] and market['active']}
+        bitforex_tickers = bitforex.fetch_tickers(list(bitforex_spot_markets.keys()))
+        return bitforex_tickers
+    except Exception as e:
+        return None, f"Error retrieving data from Bitforex API: {e}"
