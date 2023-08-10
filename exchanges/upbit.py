f3b64cecf2c51f0fa9d70d70029747e2a47fbf57
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-10 12:50:57 +0600

    Create upbit.py

---
+++ exchanges/upbit.py
@@ -0,0 +1,11 @@
+import ccxt
+
+def get_upbit_data():
+    upbit = ccxt.upbit()
+    try:
+        upbit_markets = upbit.load_markets()
+        upbit_spot_markets = {symbol: market for symbol, market in upbit_markets.items() if market['spot'] and market['active']}
+        upbit_tickers = upbit.fetch_tickers(list(upbit_spot_markets.keys()))
+        return upbit_tickers
+    except Exception as e:
+        return None, f"Error retrieving data from Upbit API: {e}"
