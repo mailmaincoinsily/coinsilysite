263f0e9124d3939d4d0bd1fb36d193433fc1403f
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-10 12:56:34 +0600

    Create korbit.py

---
+++ exchanges/korbit.py
@@ -0,0 +1,11 @@
+import ccxt
+
+def get_korbit_data():
+    korbit = ccxt.korbit()
+    try:
+        korbit_markets = korbit.load_markets()
+        korbit_spot_markets = {symbol: market for symbol, market in korbit_markets.items() if market['spot'] and market['active']}
+        korbit_tickers = korbit.fetch_tickers(list(korbit_spot_markets.keys()))
+        return korbit_tickers
+    except Exception as e:
+        return None, f"Error retrieving data from Korbit API: {e}"
