b1c80aab2a2c7dad1c87260a4a083639a609980d
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-10 12:47:15 +0600

    Create bitflyer.py

---
+++ exchanges/bitflyer.py
@@ -0,0 +1,11 @@
+import ccxt
+
+def get_bitflyer_data():
+    bitflyer = ccxt.bitflyer()
+    try:
+        bitflyer_markets = bitflyer.load_markets()
+        bitflyer_spot_markets = {symbol: market for symbol, market in bitflyer_markets.items() if market['spot'] and market['active']}
+        bitflyer_tickers = bitflyer.fetch_tickers(list(bitflyer_spot_markets.keys()))
+        return bitflyer_tickers
+    except Exception as e:
+        return None, f"Error retrieving data from Bitflyer API: {e}"
