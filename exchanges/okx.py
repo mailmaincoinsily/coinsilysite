58e86901acc718d1826db6eb73c68a9b123fd77e
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-10 12:44:05 +0600

    Create okx.py

---
+++ exchanges/okx.py
@@ -0,0 +1,11 @@
+import ccxt
+
+def get_okx_data():
+    okx = ccxt.okex()
+    try:
+        okx_markets = okx.load_markets()
+        okx_spot_markets = {symbol: market for symbol, market in okx_markets.items() if market['spot'] and market['active']}
+        okx_tickers = okx.fetch_tickers(list(okx_spot_markets.keys()))
+        return okx_tickers
+    except Exception as e:
+        return None, f"Error retrieving data from OKEx API: {e}"
