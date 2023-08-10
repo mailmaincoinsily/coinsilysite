41a3fcbf0f820960952b64aa8fe592f0aa2c085a
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-10 12:58:03 +0600

    Create coinw.py

---
+++ exchanges/coinw.py
@@ -0,0 +1,11 @@
+import ccxt
+
+def get_coinw_data():
+    coinw = ccxt.coinw()
+    try:
+        coinw_markets = coinw.load_markets()
+        coinw_spot_markets = {symbol: market for symbol, market in coinw_markets.items() if market['spot'] and market['active']}
+        coinw_tickers = coinw.fetch_tickers(list(coinw_spot_markets.keys()))
+        return coinw_tickers
+    except Exception as e:
+        return None, f"Error retrieving data from Coinw API: {e}"
