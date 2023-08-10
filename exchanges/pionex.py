f2da85f9d1c8d823a48902d333b0507b59bdc861
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-10 12:59:14 +0600

    Create pionex.py

---
+++ exchanges/pionex.py
@@ -0,0 +1,11 @@
+import ccxt
+
+def get_pionex_data():
+    pionex = ccxt.pionex()
+    try:
+        pionex_markets = pionex.load_markets()
+        pionex_spot_markets = {symbol: market for symbol, market in pionex_markets.items() if market['spot'] and market['active']}
+        pionex_tickers = pionex.fetch_tickers(list(pionex_spot_markets.keys()))
+        return pionex_tickers
+    except Exception as e:
+        return None, f"Error retrieving data from Pionex API: {e}"
