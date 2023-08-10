547be2b3803cb42e8f2b9e68af4bfec0e7fb2c8c
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-10 12:42:45 +0600

    Create kraken.py

---
+++ exchanges/kraken.py
@@ -0,0 +1,11 @@
+import ccxt
+
+def get_kraken_data():
+    kraken = ccxt.kraken()
+    try:
+        kraken_markets = kraken.load_markets()
+        kraken_spot_markets = {symbol: market for symbol, market in kraken_markets.items() if market['spot'] and market['active']}
+        kraken_tickers = kraken.fetch_tickers(list(kraken_spot_markets.keys()))
+        return kraken_tickers
+    except Exception as e:
+        return None, f"Error retrieving data from Kraken API: {e}"
