c70578223381f9efec85f1b15cebca5f79e9e1c6
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-10 12:50:34 +0600

    Create bitget.py

---
+++ exchanges/bitget.py
@@ -0,0 +1,11 @@
+import ccxt
+
+def get_bitget_data():
+    bitget = ccxt.bitget()
+    try:
+        bitget_markets = bitget.load_markets()
+        bitget_spot_markets = {symbol: market for symbol, market in bitget_markets.items() if market['spot'] and market['active']}
+        bitget_tickers = bitget.fetch_tickers(list(bitget_spot_markets.keys()))
+        return bitget_tickers
+    except Exception as e:
+        return None, f"Error retrieving data from Bitget API: {e}"
