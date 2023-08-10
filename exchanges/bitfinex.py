463f6da3c47daff21aee335c18078a34d6cba57e
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-10 12:45:50 +0600

    Create bitfinex.py

---
+++ exchanges/bitfinex.py
@@ -0,0 +1,11 @@
+import ccxt
+
+def get_bitfinex_data():
+    bitfinex = ccxt.bitfinex()
+    try:
+        bitfinex_markets = bitfinex.load_markets()
+        bitfinex_spot_markets = {symbol: market for symbol, market in bitfinex_markets.items() if market['spot'] and market['active']}
+        bitfinex_tickers = bitfinex.fetch_tickers(list(bitfinex_spot_markets.keys()))
+        return bitfinex_tickers
+    except Exception as e:
+        return None, f"Error retrieving data from Bitfinex API: {e}"
