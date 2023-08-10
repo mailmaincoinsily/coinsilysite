64c4f32048af24cc1b59855e4124be4ab398189e
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-10 12:43:17 +0600

    Create kukoin.py

---
+++ exchanges/kukoin.py
@@ -0,0 +1,11 @@
+import ccxt
+
+def get_kukoin_data():
+    kukoin = ccxt.kucoin()
+    try:
+        kukoin_markets = kukoin.load_markets()
+        kukoin_spot_markets = {symbol: market for symbol, market in kukoin_markets.items() if market['spot'] and market['active']}
+        kukoin_tickers = kukoin.fetch_tickers(list(kukoin_spot_markets.keys()))
+        return kukoin_tickers
+    except Exception as e:
+        return None, f"Error retrieving data from KuCoin API: {e}"
