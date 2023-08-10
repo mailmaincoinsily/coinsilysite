a3832ae8e471b5fc31d812982f576a9d2362c1df
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-10 12:44:30 +0600

    Create bitstamp.py

---
+++ exchanges/bitstamp.py
@@ -0,0 +1,11 @@
+import ccxt
+
+def get_bitstamp_data():
+    bitstamp = ccxt.bitstamp()
+    try:
+        bitstamp_markets = bitstamp.load_markets()
+        bitstamp_spot_markets = {symbol: market for symbol, market in bitstamp_markets.items() if market['spot'] and market['active']}
+        bitstamp_tickers = bitstamp.fetch_tickers(list(bitstamp_spot_markets.keys()))
+        return bitstamp_tickers
+    except Exception as e:
+        return None, f"Error retrieving data from Bitstamp API: {e}"
