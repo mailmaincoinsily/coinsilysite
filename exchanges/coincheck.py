9490abe6c588e2687b5b648307d40124dee87838
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-10 12:50:10 +0600

    Create coincheck.py

---
+++ exchanges/coincheck.py
@@ -0,0 +1,11 @@
+import ccxt
+
+def get_coincheck_data():
+    coincheck = ccxt.coincheck()
+    try:
+        coincheck_markets = coincheck.load_markets()
+        coincheck_spot_markets = {symbol: market for symbol, market in coincheck_markets.items() if market['spot'] and market['active']}
+        coincheck_tickers = coincheck.fetch_tickers(list(coincheck_spot_markets.keys()))
+        return coincheck_tickers
+    except Exception as e:
+        return None, f"Error retrieving data from Coincheck API: {e}"
