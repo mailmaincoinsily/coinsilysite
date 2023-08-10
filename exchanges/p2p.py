d0c49090999ef55afa729f422394a0305d967c60
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-10 12:53:53 +0600

    Create p2p.py

---
+++ exchanges/p2p.py
@@ -0,0 +1,11 @@
+import ccxt
+
+def get_p2p_data():
+    p2p = ccxt.p2p()
+    try:
+        p2p_markets = p2p.load_markets()
+        p2p_spot_markets = {symbol: market for symbol, market in p2p_markets.items() if market['spot'] and market['active']}
+        p2p_tickers = p2p.fetch_tickers(list(p2p_spot_markets.keys()))
+        return p2p_tickers
+    except Exception as e:
+        return None, f"Error retrieving data from P2P API: {e}"
