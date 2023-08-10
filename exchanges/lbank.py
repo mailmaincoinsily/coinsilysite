e60a600c7e814dc37c73c546edb22064f3d7766b
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-10 12:49:31 +0600

    Create lbank.py

---
+++ exchanges/lbank.py
@@ -0,0 +1,11 @@
+import ccxt
+
+def get_lbank_data():
+    lbank = ccxt.lbank()
+    try:
+        lbank_markets = lbank.load_markets()
+        lbank_spot_markets = {symbol: market for symbol, market in lbank_markets.items() if market['spot'] and market['active']}
+        lbank_tickers = lbank.fetch_tickers(list(lbank_spot_markets.keys()))
+        return lbank_tickers
+    except Exception as e:
+        return None, f"Error retrieving data from LBank API: {e}"
