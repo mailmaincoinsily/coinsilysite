1527abffe8e109a0a23477d44beba734f1b10118
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-11 23:17:09 +0600

    Update triangular_arbitrage.py

--- triangular_arbitrage.py
+++ triangular_arbitrage.py
@@ -1,61 +1,37 @@
 import ccxt
-import time
+import asyncio
 
-# Initialize the Binance exchange
-binance = ccxt.binance({
-    'rateLimit': 20,  # Set a reasonable rate limit (requests per second)
-})
+# Function to handle WebSocket trade events
+async def handle_trade(symbol, trade):
+    print(f"Received trade for {symbol}: {trade}")
 
-# Function to calculate triangular arbitrage opportunities
-def calculate_triangular_arbitrage():
+# Function to calculate triangular arbitrage opportunities with WebSocket
+async def calculate_triangular_arbitrage_with_ws():
     try:
+        binance = ccxt.binance()
         binance_markets = binance.load_markets()
         binance_spot_markets = {symbol: market for symbol, market in binance_markets.items() if market['spot'] and market['active']}
 
-        triangular_data = []
-
+        # Create WebSocket connections for each trading pair
+        ws_connections = []
         for base_symbol in binance_spot_markets:
             for quote_symbol in binance_spot_markets:
                 if base_symbol != quote_symbol:
-                    intermediate_symbol = None
+                    symbol = f"{base_symbol}/{quote_symbol}"
+                    ws_symbol = symbol.replace("/", "").lower()
+                    ws_url = f"wss://stream.binance.com:9443/ws/{ws_symbol}@trade"
+                    ws_connection = await binance.ws_public_subscribe(ws_url, lambda trade, symbol=symbol: handle_trade(symbol, trade))
+                    ws_connections.append(ws_connection)
 
-                    # Find the intermediate symbol
-                    for symbol in binance_spot_markets:
-                        if symbol != base_symbol and symbol != quote_symbol:
-                            intermediate_symbol = symbol
-                            break
+        while True:
+            await asyncio.sleep(1)
 
-                    if intermediate_symbol:
-                        ticker_data = binance.fetch_tickers([base_symbol, intermediate_symbol, quote_symbol])
-                        base_to_intermediate = ticker_data[base_symbol]['ask']
-                        intermediate_to_quote = ticker_data[intermediate_symbol]['ask']
-                        quote_to_base = ticker_data[quote_symbol]['bid']
+        # Close WebSocket connections
+        for ws_connection in ws_connections:
+            await ws_connection.close()
 
-                        arbitrage_ratio = (1 / base_to_intermediate) * intermediate_to_quote * quote_to_base - 1
-
-                        if arbitrage_ratio > 0.001:  # Considering fees
-                            triangular_data.append({
-                                'base_symbol': base_symbol,
-                                'intermediate_symbol': intermediate_symbol,
-                                'quote_symbol': quote_symbol,
-                                'arbitrage_ratio': round(arbitrage_ratio * 100, 2)
-                            })
-
-                        time.sleep(1)  # Add a delay to avoid exceeding rate limits
-
-        return triangular_data
     except Exception as e:
         print("Exception:", e)
-        return []
 
-# Call the function to calculate triangular arbitrage opportunities
-triangular_arbitrage_data = calculate_triangular_arbitrage()
-
-# Print the calculated triangular arbitrage opportunities
-for data in triangular_arbitrage_data:
-    print("Opportunity:")
-    print("Base Symbol:", data['base_symbol'])
-    print("Intermediate Symbol:", data['intermediate_symbol'])
-    print("Quote Symbol:", data['quote_symbol'])
-    print("Arbitrage Ratio:", data['arbitrage_ratio'], "%")
-    print()
+# Call the function to calculate triangular arbitrage opportunities with WebSocket
+asyncio.get_event_loop().run_until_complete(calculate_triangular_arbitrage_with_ws())
