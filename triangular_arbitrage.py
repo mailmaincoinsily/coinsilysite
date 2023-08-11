04ce7ffeada954a058de78356fcb5e066f978f48
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-11 21:37:39 +0600

    Update triangular_arbitrage.py

--- triangular_arbitrage.py
+++ triangular_arbitrage.py
@@ -5,6 +5,7 @@ binance = ccxt.binance()
 
 # Function to calculate triangular arbitrage opportunities
 def calculate_triangular_arbitrage():
+    print("Function is being executed")
     try:
         binance_markets = binance.load_markets()
         binance_spot_markets = {symbol: market for symbol, market in binance_markets.items() if market['spot'] and market['active']}
@@ -12,13 +13,7 @@ def calculate_triangular_arbitrage():
         triangular_data = []
 
         for symbol in binance_spot_markets:
-            print("Processing symbol:", symbol)  # Debug line
-            parts = symbol.split('/')
-            if len(parts) != 3:
-                print("Skipping invalid symbol:", symbol)  # Debug line
-                continue
-
-            base, _, quote = parts
+            base, _, quote = symbol.split('/')
             reverse_symbol = f"{quote}/{base}"
             
             if reverse_symbol in binance_spot_markets:
@@ -26,12 +21,33 @@ def calculate_triangular_arbitrage():
                 quote_market = binance_spot_markets[reverse_symbol]
                 
                 if quote_market['quote'] == quote:
-                    arbitrage = (1 / base_market['bid']) * quote_market['ask'] * base_market['ask'] - 1
+                    print(f"Processing symbol: {symbol}")
+                    print(f"Base Market: {base_market}")
+                    print(f"Quote Market: {quote_market}")
+                    
+                    base_bid = base_market['bid']
+                    quote_ask = quote_market['ask']
+                    base_ask = base_market['ask']
+                    
+                    print(f"Base Bid: {base_bid}, Quote Ask: {quote_ask}, Base Ask: {base_ask}")
+                    
+                    arbitrage = (1 / base_bid) * quote_ask * base_ask - 1
+                    print(f"Calculated Arbitrage: {arbitrage}")
+                    
                     triangular_data.append({'symbol': symbol, 'profit': round(arbitrage * 100, 2)})
-
+                    print(f"Added to triangular_data: {triangular_data[-1]}")
+                    
         return triangular_data
     except Exception as e:
-        print("Exception:", e)  # Debug line
+        print(f"Exception: {e}")
         return []
 
 # You can add more functions related to triangular arbitrage here if needed
+
+if __name__ == "__main__":
+    print("Triangular Arbitrage Opportunities")
+    results = calculate_triangular_arbitrage()
+    print("Symbol\tProfit (%)")
+    for result in results:
+        print(f"{result['symbol']}\t{result['profit']}")
+
