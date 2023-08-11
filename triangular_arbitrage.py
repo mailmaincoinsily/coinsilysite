848a9010f22fcfda52e58460a788b077994bf824
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-11 23:03:29 +0600

    Update triangular_arbitrage.py

--- triangular_arbitrage.py
+++ triangular_arbitrage.py
@@ -1,35 +1,38 @@
 import ccxt
-import concurrent.futures
+import time
 
 # Initialize the Binance exchange
-binance = ccxt.binance()
-binance_markets = binance.load_markets()
-binance_spot_markets = {symbol: market for symbol, market in binance_markets.items() if market['spot'] and market['active']}
-
-# Function to fetch ticker data
-def fetch_ticker_data(symbol):
-    return symbol, binance.fetch_ticker(symbol)
-
-# Function to calculate arbitrage ratio
-def calculate_arbitrage_ratio(base_data, intermediate_data, quote_data):
-    base_to_intermediate = base_data['ask']
-    intermediate_to_quote = intermediate_data['ask']
-    quote_to_base = quote_data['bid']
-    return (1 / base_to_intermediate) * intermediate_to_quote * quote_to_base - 1
+binance = ccxt.binance({
+    'rateLimit': 20,  # Set a reasonable rate limit (requests per second)
+})
 
 # Function to calculate triangular arbitrage opportunities
 def calculate_triangular_arbitrage():
-    triangular_data = []
-    
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        ticker_data = executor.map(fetch_ticker_data, binance_spot_markets.keys())
-    
-    for base_symbol, base_data in ticker_data:
-        for intermediate_symbol, intermediate_data in ticker_data:
-            if base_symbol != intermediate_symbol:
-                for quote_symbol, quote_data in ticker_data:
-                    if quote_symbol != base_symbol and quote_symbol != intermediate_symbol:
-                        arbitrage_ratio = calculate_arbitrage_ratio(base_data, intermediate_data, quote_data)
+    try:
+        binance_markets = binance.load_markets()
+        binance_spot_markets = {symbol: market for symbol, market in binance_markets.items() if market['spot'] and market['active']}
+
+        triangular_data = []
+
+        for base_symbol in binance_spot_markets:
+            for quote_symbol in binance_spot_markets:
+                if base_symbol != quote_symbol:
+                    intermediate_symbol = None
+
+                    # Find the intermediate symbol
+                    for symbol in binance_spot_markets:
+                        if symbol != base_symbol and symbol != quote_symbol:
+                            intermediate_symbol = symbol
+                            break
+
+                    if intermediate_symbol:
+                        ticker_data = binance.fetch_tickers([base_symbol, intermediate_symbol, quote_symbol])
+                        base_to_intermediate = ticker_data[base_symbol]['ask']
+                        intermediate_to_quote = ticker_data[intermediate_symbol]['ask']
+                        quote_to_base = ticker_data[quote_symbol]['bid']
+
+                        arbitrage_ratio = (1 / base_to_intermediate) * intermediate_to_quote * quote_to_base - 1
+
                         if arbitrage_ratio > 0.001:  # Considering fees
                             triangular_data.append({
                                 'base_symbol': base_symbol,
@@ -37,8 +40,13 @@ def calculate_triangular_arbitrage():
                                 'quote_symbol': quote_symbol,
                                 'arbitrage_ratio': round(arbitrage_ratio * 100, 2)
                             })
-    
-    return triangular_data
+
+                        time.sleep(1)  # Add a delay to avoid exceeding rate limits
+
+        return triangular_data
+    except Exception as e:
+        print("Exception:", e)
+        return []
 
 # Call the function to calculate triangular arbitrage opportunities
 triangular_arbitrage_data = calculate_triangular_arbitrage()
