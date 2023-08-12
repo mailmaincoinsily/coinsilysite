5d69783c2eb6044d574654e9f280592f2239ea1f
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-12 19:49:37 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,49 @@
 import ccxt
 
-def find_arbitrage_opportunities():
+async def find_triangular_arbitrage_opportunities():
     binance = ccxt.binance()
-    markets = binance.fetch_markets()
     
-    usdt_assets = set()
+    markets = await binance.load_markets()
+    usdt_markets = [market for market in markets if market.endswith('/USDT')]
     
-    # Collect assets trading with USDT
-    for market in markets:
-        if 'USDT' in market['symbol']:
-            base, quote = market['symbol'].split('/')
-            if base != 'USDT':
-                usdt_assets.add(base)
-            if quote != 'USDT':
-                usdt_assets.add(quote)
+    profitable_trades = []
     
-    usdt_assets = list(usdt_assets)
-    
-    for base in usdt_assets:
-        for quote in usdt_assets:
-            if base != quote:
-                pair1 = f"{base}/USDT"
-                pair2 = f"USDT/{quote}"
-                pair3 = f"{quote}/{base}"
+    for market in usdt_markets:
+        base_currency, _ = market.split('/')
+        
+        for intermediate_currency in markets:
+            if intermediate_currency.endswith('/USDT') or intermediate_currency == market:
+                continue
+            
+            try:
+                order_book = await binance.fetch_order_book(market)
+                bid_price = order_book['bids'][0][0]
                 
-                try:
-                    orderbook1 = binance.fetch_order_book(pair1)
-                    orderbook2 = binance.fetch_order_book(pair2)
-                    orderbook3 = binance.fetch_order_book(pair3)
-                    
-                    bid1 = orderbook1['bids'][0][0] if len(orderbook1['bids']) > 0 else None
-                    ask1 = orderbook1['asks'][0][0] if len(orderbook1['asks']) > 0 else None
-                    bid2 = orderbook2['bids'][0][0] if len(orderbook2['bids']) > 0 else None
-                    ask2 = orderbook2['asks'][0][0] if len(orderbook2['asks']) > 0 else None
-                    bid3 = orderbook3['bids'][0][0] if len(orderbook3['bids']) > 0 else None
-                    ask3 = orderbook3['asks'][0][0] if len(orderbook3['asks']) > 0 else None
-                    
-                    if bid1 and ask2 and ask3:
-                        rate = (1 / bid1) * ask2 * ask3
-                        if rate > 1:
-                            print(f"Arbitrage Opportunity Found: {pair1} -> {pair2} -> {pair3}")
-                            print(f"Profit Rate: {rate:.8f}")
-                            print("======================")
-                except:
-                    pass
+                intermediate_order_book = await binance.fetch_order_book(intermediate_currency)
+                ask_price = intermediate_order_book['asks'][0][0]
+                
+                arbitrage_percentage = ((ask_price / bid_price) - 1) * 100
+                
+                if arbitrage_percentage > 0:
+                    profitable_trades.append({
+                        'pair1': market,
+                        'pair2': intermediate_currency,
+                        'arbitrage_percentage': arbitrage_percentage
+                    })
+            except (ccxt.BaseError, IndexError):
+                pass
+            
+    profitable_trades.sort(key=lambda trade: trade['arbitrage_percentage'], reverse=True)
+    
+    return profitable_trades
 
-find_arbitrage_opportunities()
+async def main():
+    opportunities = await find_triangular_arbitrage_opportunities()
+    
+    if opportunities:
+        for opportunity in opportunities:
+            print(f"Opportunity: {opportunity['pair1']} -> {opportunity['pair2']} ({opportunity['arbitrage_percentage']:.2f}%)")
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
