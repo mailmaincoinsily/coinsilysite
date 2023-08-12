3bb8bc2f2c10fe242330e09ffd4bb7bb72acca3b
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-12 19:08:12 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import ccxt
+import concurrent.futures
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -18,38 +19,31 @@ base_currencies = ['BTC', 'BNB', 'USDT']
 # List to store arbitrage opportunities
 arbitrage_opportunities = []
 
+# Function to fetch ticker data for a pair
+def fetch_ticker(pair):
+    ticker = binance.fetch_ticker(pair)
+    return ticker
+
 # Loop through all pairs involving base currencies
-for base_currency in base_currencies:
-    for pair1 in all_pairs:
-        if base_currency in pair1:
-            for pair2 in all_pairs:
-                if base_currency in pair2:
-                    for pair3 in all_pairs:
-                        if pair1 != pair2 and pair2 != pair3 and pair3 != pair1 and base_currency in pair3:
-                            ticker1 = binance.fetch_ticker(pair1)
-                            ticker2 = binance.fetch_ticker(pair2)
-                            ticker3 = binance.fetch_ticker(pair3)
-
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    futures = []
+    for base_currency in base_currencies:
+        for pair1 in all_pairs:
+            if base_currency in pair1:
+                for pair2 in all_pairs:
+                    if base_currency in pair2:
+                        for pair3 in all_pairs:
                             if (
-                                ticker1['ask'] and ticker1['bid'] and
-                                ticker2['ask'] and ticker2['bid'] and
-                                ticker3['ask'] and ticker3['bid']
+                                pair1 != pair2 and pair2 != pair3 and pair3 != pair1 and
+                                base_currency in pair3
                             ):
-                                rate1_to_2 = ticker1['ask']
-                                rate2_to_3 = 1 / ticker2['bid']
-                                rate3_to_1 = 1 / ticker3['bid']
+                                futures.append(executor.submit(fetch_ticker, pair1))
+                                futures.append(executor.submit(fetch_ticker, pair2))
+                                futures.append(executor.submit(fetch_ticker, pair3))
 
-                                potential_profit_percentage = (
-                                    rate1_to_2 * rate2_to_3 * rate3_to_1
-                                ) * 100 - 100
-
-                                if potential_profit_percentage > 0:
-                                    arbitrage_opportunities.append({
-                                        'pair1': pair1,
-                                        'pair2': pair2,
-                                        'pair3': pair3,
-                                        'potential_profit_percentage': potential_profit_percentage
-                                    })
+    for future in concurrent.futures.as_completed(futures):
+        ticker = future.result()
+        # Perform calculations and store arbitrage opportunities
 
 # Sort arbitrage opportunities by potential profit percentage
 arbitrage_opportunities.sort(key=lambda x: x['potential_profit_percentage'], reverse=True)
