6973823f0f1e75bbd50bfadf7ec89f974b2ba79b
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-12 19:02:08 +0600

    Update app.py

--- app.py
+++ app.py
@@ -18,28 +18,41 @@ base_currencies = ['BTC', 'BNB', 'USDT']
 # List to store arbitrage opportunities
 arbitrage_opportunities = []
 
-# Loop through all pairs
-for pair in all_pairs:
-    base_currency = pair.split('/')[0]
-    quote_currency = pair.split('/')[1]
+# Loop through all pairs involving base currencies
+for base_currency in base_currencies:
+    for pair1 in all_pairs:
+        if base_currency in pair1:
+            for pair2 in all_pairs:
+                if base_currency in pair2:
+                    for pair3 in all_pairs:
+                        if pair1 != pair2 and pair2 != pair3 and pair3 != pair1 and base_currency in pair3:
+                            ticker1 = binance.fetch_ticker(pair1)
+                            ticker2 = binance.fetch_ticker(pair2)
+                            ticker3 = binance.fetch_ticker(pair3)
 
-    if base_currency in base_currencies and quote_currency != base_currency:
-        ticker = binance.fetch_ticker(pair)
+                            if (
+                                ticker1['ask'] and ticker1['bid'] and
+                                ticker2['ask'] and ticker2['bid'] and
+                                ticker3['ask'] and ticker3['bid']
+                            ):
+                                rate1_to_2 = ticker1['ask']
+                                rate2_to_3 = 1 / ticker2['bid']
+                                rate3_to_1 = 1 / ticker3['bid']
 
-        if ticker['ask'] and ticker['bid']:
-            rate_base_to_quote = ticker['ask']
-            rate_quote_to_base = 1 / ticker['bid']
+                                potential_profit_percentage = (
+                                    rate1_to_2 * rate2_to_3 * rate3_to_1
+                                ) * 100 - 100
 
-            profit_percentage = (rate_base_to_quote * rate_quote_to_base) * 100 - 100
+                                if potential_profit_percentage > 0:
+                                    arbitrage_opportunities.append({
+                                        'pair1': pair1,
+                                        'pair2': pair2,
+                                        'pair3': pair3,
+                                        'potential_profit_percentage': potential_profit_percentage
+                                    })
 
-            if profit_percentage > 0:
-                arbitrage_opportunities.append({
-                    'pair': pair,
-                    'profit_percentage': profit_percentage
-                })
-
-# Sort arbitrage opportunities by profit percentage
-arbitrage_opportunities.sort(key=lambda x: x['profit_percentage'], reverse=True)
+# Sort arbitrage opportunities by potential profit percentage
+arbitrage_opportunities.sort(key=lambda x: x['potential_profit_percentage'], reverse=True)
 
 @app.route('/')
 def index():
