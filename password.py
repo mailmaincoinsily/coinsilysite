f6b8682ebfb068646bf9e7ed980a09779bb0a58d
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-14 20:39:45 +0600

    Update password.py

--- password.py
+++ password.py
@@ -1,11 +1,10 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from werkzeug.security import generate_password_hash
-from yourapp import db  # Import your SQLAlchemy instance
 
-password_bp = Blueprint('password', __name__)
+password_bp = Blueprint('password', __name__, url_prefix='/password')
 
-@password_bp.route('/set_password', methods=['GET', 'POST'])
+@password_bp.route('/set', methods=['GET', 'POST'])
 @login_required
 def set_password():
     if request.method == 'POST':
@@ -13,12 +12,11 @@ def set_password():
         confirm_password = request.form['confirm_password']
 
         if new_password == confirm_password:
-            hashed_password = generate_password_hash(new_password)
-            current_user.password_hash = hashed_password
+            current_user.password_hash = generate_password_hash(new_password)
             db.session.commit()
-            flash('Password updated successfully!', 'success')
-            return redirect(url_for('profile'))  # Redirect to profile or another appropriate page
+            flash('Password successfully set!', 'success')
+            return redirect(url_for('index'))
         else:
-            flash('Passwords do not match', 'error')
+            flash('Passwords do not match.', 'error')
 
-    return render_template('password_setting.html')
+    return render_template('password.html')
