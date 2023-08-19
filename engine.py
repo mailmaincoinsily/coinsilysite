8791b1af848a04c84b0b6caaf1af06e0ae89efdb
alamintestbot <140479103+alamintestbot@users.noreply.github.com>
2023-08-19 22:51:11 +0600

    Update engine.py

--- engine.py
+++ engine.py
@@ -37,7 +37,7 @@ def calculate_arbitrage(exchange1, exchange2):
             symbol_link = symbol.replace('/', '_')
         elif exchange1 == 'exchanges.bybit':
             symbol_link = symbol.replace('/', '/')
-        elif exchange1 == 'exchanges.kukoin':
+        elif exchange1 == 'exchanges.kucoin':
             symbol_link = symbol.replace('/', '-')
         else:
             symbol_link = symbol
